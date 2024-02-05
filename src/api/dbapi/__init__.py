5cb4f071088d8c17943041107cbbb64b8cd0d1cd
selfdestroying <max.f99@yandex.ru>
2024-02-05 10:17:12 +0300

    ~BIG UPDATE~ add __init__ files

---
+++ src/api/dbapi/__init__.py
@@ -0,0 +1,3 @@
+from src.api.dbapi import child as childapi  # noqa: F401
+from src.api.dbapi import schedule as scheduleapi  # noqa: F401
+from src.api.dbapi import user as userapi  # noqa: F401
---
+++ src/bot/handlers/__init__.py
@@ -0,0 +1,15 @@
+from src.bot.handlers.day import router as day_router
+from src.bot.handlers.night import router as night_router
+from src.bot.handlers.profile import router as profile_router
+from src.bot.handlers.register import router as register_router
+from src.bot.handlers.start import router as start_router
+from src.bot.handlers.stats import router as stats_router
+
+routers = (
+    start_router,
+    register_router,
+    day_router,
+    night_router,
+    stats_router,
+    profile_router,
+)
